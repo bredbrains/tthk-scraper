b993244f99fbebefc10af3f68d355243b75b0372
Nikolas Laus <bchik3@gmail.com>
2021-11-14 00:11:22 +0200

    Move table name in BaseCachedClient to property.

--- tthk_scraper/clients/base_cached_client.py
+++ tthk_scraper/clients/base_cached_client.py
@@ -21,6 +21,10 @@ class BaseCachedClient:
         self.parser_client = parser_client
         self.updates_database_client = UpdateService()
 
+    @property
+    def table(self):
+        return self.model.__tablename__
+
     def get(self):
         data = self.service.get_all()
         is_deprecated = self.is_deprecated()
@@ -31,18 +35,17 @@ class BaseCachedClient:
     def get_new(self, is_deprecated: bool = False):
         document = BrowserClient(self.url).open_page()
         data = self.parser_client.parse(document)
-        self.updates_database_client.save(self.model.__tablename__)
+        self.updates_database_client.save(self.table)
         self.service.update(data) if is_deprecated else self.service.save(data)
         return data
 
     def is_deprecated(self):
-        table = self.model.__tablename__
-        update_time = self.updates_database_client.get_by_table(table)
+        update_time = self.updates_database_client.get_by_table(self.table)
         if update_time is None:
             return True
         current_time = datetime.now()
         update_diff = current_time - update_time.timestamp
-        return update_diff.total_seconds() / 3600 > DEPRECATION_TIME[table]
+        return update_diff.total_seconds() / 3600 > DEPRECATION_TIME[self.table]
 
     def save(self, models):
         self.service.save(models)
