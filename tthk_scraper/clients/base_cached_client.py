4a9c699d0ff6fbe3c7b609f01103abcbf3292fea
Nikolas Laus <bchik3@gmail.com>
2021-11-24 17:05:14 +0200

    Add TODO extend class to parse multiple URLs in BaseCachedClient.

--- tthk_scraper/clients/base_cached_client.py
+++ tthk_scraper/clients/base_cached_client.py
@@ -4,12 +4,13 @@ from typing import Type
 from sqlmodel import SQLModel
 
 from tthk_scraper.clients.browser_client import BrowserClient
+from tthk_scraper.clients.parsers.base_parser_client import BaseParserClient
 from tthk_scraper.services import BaseService
 from tthk_scraper.services.update_service import UpdateService
-from tthk_scraper.clients.parsers.base_parser_client import BaseParserClient
 from tthk_scraper.utils.api_globals import DEPRECATION_TIME
 
 
+# TODO: Extend to parse multiple URLs in case of consultations and teachers
 class BaseCachedClient:
     def __init__(self, model: Type[SQLModel],
                  url: str,
@@ -22,7 +23,7 @@ class BaseCachedClient:
         self.updates_database_client = UpdateService()
 
     @property
-    def table(self):
+    def tablename(self):
         return self.model.__tablename__
 
     def get(self):
@@ -35,17 +36,17 @@ class BaseCachedClient:
     def get_new(self, is_deprecated: bool = False):
         document = BrowserClient(self.url).open_page()
         data = self.parser_client.parse(document)
-        self.updates_database_client.save(self.table)
+        self.updates_database_client.save(self.tablename)
         self.service.update(data) if is_deprecated else self.service.save(data)
         return data
 
     def is_deprecated(self):
-        update_time = self.updates_database_client.get_by_table(self.table)
+        update_time = self.updates_database_client.get_by_table(self.tablename)
         if update_time is None:
             return True
         current_time = datetime.now()
         update_diff = current_time - update_time.timestamp
-        return update_diff.total_seconds() / 3600 > DEPRECATION_TIME[self.table]
+        return update_diff.total_seconds() / 3600 > DEPRECATION_TIME[self.tablename]
 
     def save(self, models):
         self.service.save(models)
