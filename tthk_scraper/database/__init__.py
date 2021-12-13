4c8f793e076136c82f64c8a51bf8edc96eeebe52
Nikolas Laus <bchik3@gmail.com>
2021-12-13 15:20:30 +0200

    Move database credentials to environment variables.

--- tthk_scraper/database/__init__.py
+++ tthk_scraper/database/__init__.py
@@ -1,6 +1,14 @@
+import os
+
 from sqlmodel import create_engine, SQLModel, Session
 
-DATABASE_URL = "mysql+pymysql://root:mysql@localhost:3306/tthk_scraper"
+MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_PORT = os.getenv('MYSQL_PORT')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
 engine = create_engine(
     DATABASE_URL
